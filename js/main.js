f9a86956d85c0c80031b0be8aff5a2f2c941350a
JOAO TARRAN <joaotarran@gmail.com>
2022-11-23 18:02:48 -0300

    Update Solucoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Hamburger e Menu Secundário
+
 const burger = document.getElementById("burger01");
 const nav2 = document.getElementById("nav-secondary-id");
 let nav2isOpen = false;
@@ -13,5 +15,98 @@ burger.addEventListener("click", () => {
         burger.classList.remove("burger-active");
         nav2isOpen = false;
         console.log("secondary nav closed");
+    }
+});
+
+// Menu de Soluções
+
+const myAnchor1 = document.getElementById("anchor-1");
+const myAnchor2 = document.getElementById("anchor-2");
+const myAnchor3 = document.getElementById("anchor-3");
+const myAnchor4 = document.getElementById("anchor-4");
+
+const scrollContainer = document.querySelector(".video-container");
+
+myAnchor1.addEventListener("click", (event) => {
+    // Prevents the link's default behavior:
+    event.preventDefault();
+
+    // Gives us the string: "#my-element":
+    const link = $(myAnchor1).attr("href");
+
+    // Finds the corresponding element:
+    const element = document.querySelector(link);
+
+    if (element) {
+        const leftPosition = element.offsetLeft;
+
+        // This line actually takes us to the element:
+        document.getElementById("video-container-01").scrollLeft = leftPosition;
+        console.log("feito");
+    } else {
+        console.log("não feito");
+    }
+});
+
+myAnchor2.addEventListener("click", (event) => {
+    // Prevents the link's default behavior:
+    event.preventDefault();
+
+    // Gives us the string: "#my-element":
+    const link = $(myAnchor2).attr("href");
+
+    // Finds the corresponding element:
+    const element = document.querySelector(link);
+
+    if (element) {
+        const leftPosition = element.offsetLeft;
+
+        // This line actually takes us to the element:
+        document.getElementById("video-container-01").scrollLeft = leftPosition;
+        console.log("feito");
+    } else {
+        console.log("não feito");
+    }
+});
+
+myAnchor3.addEventListener("click", (event) => {
+    // Prevents the link's default behavior:
+    event.preventDefault();
+
+    // Gives us the string: "#my-element":
+    const link = $(myAnchor3).attr("href");
+
+    // Finds the corresponding element:
+    const element = document.querySelector(link);
+
+    if (element) {
+        const leftPosition = element.offsetLeft;
+
+        // This line actually takes us to the element:
+        document.getElementById("video-container-01").scrollLeft = leftPosition;
+        console.log("feito");
+    } else {
+        console.log("não feito");
+    }
+});
+
+myAnchor4.addEventListener("click", (event) => {
+    // Prevents the link's default behavior:
+    event.preventDefault();
+
+    // Gives us the string: "#my-element":
+    const link = $(myAnchor4).attr("href");
+
+    // Finds the corresponding element:
+    const element = document.querySelector(link);
+
+    if (element) {
+        const leftPosition = element.offsetLeft;
+
+        // This line actually takes us to the element:
+        document.getElementById("video-container-01").scrollLeft = leftPosition;
+        console.log("feito");
+    } else {
+        console.log("não feito");
     }
 });
