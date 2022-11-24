68456b5b43929f72e09a2cca44d3b2eafbfca849
JOAO TARRAN <joaotarran@gmail.com>
2022-11-23 22:24:40 -0300

    Card Flip + Solutions Update

--- js/main.js
+++ js/main.js
@@ -24,7 +24,10 @@ const prodAnchor1 = document.getElementById("prod-anchor-1");
 const prodAnchor2 = document.getElementById("prod-anchor-2");
 
 const prodContainer1 = document.getElementById("prod-container-1");
+
 const prodCard1 = document.getElementById("prod-card-1");
+const prodCard2 = document.getElementById("prod-card-2");
+const prodCard3 = document.getElementById("prod-card-3");
 
 let cardWidth = prodCard1.clientWidth;
 
@@ -38,12 +41,38 @@ prodAnchor2.addEventListener("click", (event) => {
     prodContainer1.scrollLeft -= cardWidth;
 });
 
+// Card Flip
+
+const flip_btn1 = document.getElementById("flip-btn-1");
+const flip_btn2 = document.getElementById("flip-btn-2");
+const flip_btn3 = document.getElementById("flip-btn-3");
+
+flip_btn1.addEventListener("click", (event) => {
+    event.preventDefault();
+    prodCard1.classList.toggle("card-active");
+});
+
+flip_btn2.addEventListener("click", (event) => {
+    event.preventDefault();
+    prodCard2.classList.toggle("card-active");
+});
+
+flip_btn3.addEventListener("click", (event) => {
+    event.preventDefault();
+    prodCard3.classList.toggle("card-active");
+});
+
 // Menu de Soluções
 
 const myAnchor1 = document.getElementById("anchor-1");
 const myAnchor2 = document.getElementById("anchor-2");
 const myAnchor3 = document.getElementById("anchor-3");
 const myAnchor4 = document.getElementById("anchor-4");
+
+let anchor1_open = true;
+let anchor2_open = false;
+let anchor3_open = false;
+let anchor4_open = false;
 
 const scrollContainer = document.querySelector(".video-container");
 
@@ -62,6 +91,19 @@ myAnchor1.addEventListener("click", (event) => {
 
         // This line actually takes us to the element:
         document.getElementById("video-container-01").scrollLeft = leftPosition;
+
+        if (anchor1_open === false) {
+            myAnchor1.classList.add("solution-active");
+            anchor1_open = true;
+        }
+
+        myAnchor2.classList.remove("solution-active");
+        anchor2_open = false;
+        myAnchor3.classList.remove("solution-active");
+        anchor3_open = false;
+        myAnchor4.classList.remove("solution-active");
+        anchor4_open = false;
+
         console.log("feito");
     } else {
         console.log("não feito");
@@ -83,6 +125,19 @@ myAnchor2.addEventListener("click", (event) => {
 
         // This line actually takes us to the element:
         document.getElementById("video-container-01").scrollLeft = leftPosition;
+
+        if (anchor2_open === false) {
+            myAnchor2.classList.add("solution-active");
+            anchor2_open = true;
+        }
+
+        myAnchor1.classList.remove("solution-active");
+        anchor1_open = false;
+        myAnchor3.classList.remove("solution-active");
+        anchor3_open = false;
+        myAnchor4.classList.remove("solution-active");
+        anchor4_open = false;
+
         console.log("feito");
     } else {
         console.log("não feito");
@@ -104,6 +159,19 @@ myAnchor3.addEventListener("click", (event) => {
 
         // This line actually takes us to the element:
         document.getElementById("video-container-01").scrollLeft = leftPosition;
+
+        if (anchor3_open === false) {
+            myAnchor3.classList.add("solution-active");
+            anchor3_open = true;
+        }
+
+        myAnchor1.classList.remove("solution-active");
+        anchor1_open = false;
+        myAnchor2.classList.remove("solution-active");
+        anchor2_open = false;
+        myAnchor4.classList.remove("solution-active");
+        anchor4_open = false;
+
         console.log("feito");
     } else {
         console.log("não feito");
@@ -125,6 +193,19 @@ myAnchor4.addEventListener("click", (event) => {
 
         // This line actually takes us to the element:
         document.getElementById("video-container-01").scrollLeft = leftPosition;
+
+        if (anchor4_open === false) {
+            myAnchor4.classList.add("solution-active");
+            anchor4_open = true;
+        }
+
+        myAnchor1.classList.remove("solution-active");
+        anchor1_open = false;
+        myAnchor2.classList.remove("solution-active");
+        anchor2_open = false;
+        myAnchor3.classList.remove("solution-active");
+        anchor3_open = false;
+
         console.log("feito");
     } else {
         console.log("não feito");
