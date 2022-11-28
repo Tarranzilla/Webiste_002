c627d73599a966de4fa892071ad573d96d3fd8d1
JOAO TARRAN <joaotarran@gmail.com>
2022-11-27 22:35:59 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ burger.addEventListener("click", () => {
     if (nav2isOpen === false) {
         nav2.classList.add("nav-secondary-active");
         burger.classList.add("burger-active");
+        document.body.classList.add("body-overflow");
         nav2isOpen = true;
         console.log("secondary nav open");
     } else {
         nav2.classList.remove("nav-secondary-active");
         burger.classList.remove("burger-active");
+        document.body.classList.remove("body-overflow");
         nav2isOpen = false;
         console.log("secondary nav closed");
     }
