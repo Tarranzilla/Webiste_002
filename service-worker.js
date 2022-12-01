dd46ad83ee6d4429fa382c91a44b82636313af1a
JOAO TARRAN <joaotarran@gmail.com>
2022-11-30 22:40:32 -0300

    Service Workers Working

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ importScripts(
     "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
+workbox.setConfig({
+    debug: true,
+});
+
 workbox.routing.registerRoute(
     ({ request }) => request.destination === "image",
     new workbox.strategies.CacheFirst()
 );
-
-workbox.setConfig({
-    debug: true,
-});
